1c83b4d9ab997ef7dab95146823649bc87cf2b1a
ahmedmaher134 <a.maher.zahran@gmail.com>
2026-06-04 10:58:21 +0300

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,6 +1,6 @@
 # Import python packages.
 import streamlit as st
-from snowflake.snowpark.functions import col
+streamlit.title('My Parents New Healthy Diner')
 # Write directly to the app.
 st.title(f" :tropical_drink: Customize Your Smoothie :cup_with_straw:")
 st.write(
